1b24f6833e4c308860153fb331b8615502bf91b0
noname55804 <marlin2201@gmail.com>
2014-08-31 14:53:54 +0200

    delete system/hackapp....really

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -208,6 +208,10 @@ class EdifyGenerator(object):
     cmd = "delete(" + ",\0".join(['"%s"' % (i,) for i in file_list]) + ");"
     self.script.append(self._WordWrap(cmd))
 
+  def DeletePath(self);
+	"""delete a whole directory"""
+	self.script.append('delete_recursive("%s");')
+	
   def RenameFile(self, srcfile, tgtfile):
     """Moves a file from one location to another."""
     if self.info.get("update_rename_support", False):
